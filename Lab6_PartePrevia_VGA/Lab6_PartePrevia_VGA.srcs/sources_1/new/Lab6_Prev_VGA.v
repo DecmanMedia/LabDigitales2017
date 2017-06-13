4c1cb006c1c84ced57ba56fac00b1654e81a99d0
DecmanMedia <diego.martinez.12@sansano.usm.cl>
2017-06-13 16:37:11 -0400

    Lab6 Parte Previa_VGA
    Implementancion del temple del github

---
+++ Lab6_PartePrevia_VGA/Lab6_PartePrevia_VGA.srcs/sources_1/new/Lab6_Prev_VGA.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 13.06.2017 17:11:08
+// Design Name: 
+// Module Name: Lab6_Prev_VGA
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module Lab6_Prev_VGA(
+
+    );
+endmodule
